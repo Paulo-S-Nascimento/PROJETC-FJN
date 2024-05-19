5cf812a7ea9e2953d7364d3df9de885318ab98f3
Paulo Nascimento <ppnascimentoo@hotmail.com>
2024-05-19 02:16:05 -0300

    PROJECT-FJN

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+
+console.log('Tailwind CSS config loaded');
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ['/src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js}'], 
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
